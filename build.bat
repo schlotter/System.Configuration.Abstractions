89ba4f4d05bc0e16ee6c26a905507f1e5f3b76dc
Christian Schlotter <again@gmx.de>
2020-01-07 14:58:54 +0100

    Update build.bat to support several versions of Visual Studio 2017 and 2019.

--- build.bat
+++ build.bat
@@ -1,1 +1,33 @@
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" /m:8 /p:Configuration=Release "System.Configuration.Abstractions.sln"
+set "VSBASEDIR=%ProgramFiles(x86)%\Microsoft Visual Studio"
+set "MSBENT2019=%VSBASEDIR%\2019\Enterprise\MSBuild\Current\Bin\msbuild.exe"
+set "MSBPRO2019=%VSBASEDIR%\2019\Professional\MSBuild\Current\Bin\msbuild.exe"
+set "MSBCOM2019=%VSBASEDIR%\2019\Community\MSBuild\Current\Bin\msbuild.exe"
+set "MSBENT2017=%VSBASEDIR%\2017\Enterprise\MSBuild\15.0\Bin\msbuild.exe"
+set "MSBPRO2017=%VSBASEDIR%\2017\Professional\MSBuild\15.0\Bin\msbuild.exe"
+set "MSBCOM2017=%VSBASEDIR%\2017\Community\MSBuild\15.0\Bin\msbuild.exe"
+
+if exist "%MSBENT2019%" (
+  SET "MSBUILDEXE=%MSBENT2019%"
+) else (
+  if exist "%MSBPRO2019%" (
+    SET "MSBUILDEXE=%MSBPRO2019%"
+  ) else (
+    if exist "%MSBCOM2019%" (
+      SET "MSBUILDEXE=%MSBCOM2019%"
+    ) else (
+      if exist "%MSBENT2017%" (
+        SET "MSBUILDEXE=%MSBENT2017%"
+      ) else (
+        if exist "%MSBPRO2017%" (
+          SET "MSBUILDEXE=%MSBPRO2017%"
+        ) else (
+          if exist "%MSBCOM2017%" (
+            SET "MSBUILDEXE=%MSBCOM2017%"
+          )
+        )
+      )
+    )
+  )
+)
+
+"%MSBUILDEXE%" /m:8 /p:Configuration=Release "System.Configuration.Abstractions.sln"
